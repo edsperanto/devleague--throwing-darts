a9c34f271363a7b7f3af7364b4bb90d494fcf730
Edward Gao <egao@sent.com>
2017-02-18 10:35:59 -1000

    simplify isValidArr

--- isValidArr.js
+++ isValidArr.js
@@ -2,7 +2,7 @@ module.exports = function(arr) {
 	if(arr instanceof Array === false) return false;
 	for(let i = 0; i < arr.length; i++) {
 		if(isNaN(Number(arr[i]))) return false;
-		if(typeof arr[i] === 'boolean' || arr[i] === null) return false;
+		if(typeof arr[i] !== 'number') return false;
 	}
 	return true;
 }
