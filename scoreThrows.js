ceafc0b5965da827a6fc211d59ae8765e6868744
Edward Gao <egao@sent.com>
2017-02-18 09:37:11 -1000

    how I think it should work

---
+++ scoreThrows.js
@@ -0,0 +1,12 @@
+module.exports = function(arr) {
+	return arr.reduce((prev, rad, idx) => {
+		if(rad < 5) prev.pts += 10;
+		if(rad >= 5 && rad <= 10) prev.pts += 5;
+		if(rad > 10) prev.perfect = false;
+		if(idx = arr.length - 1) {
+			if(prev.perfect) prev.pts += 100;
+			return prev.pts;
+		}
+		return prev;
+	}, {pts: 0, perfect: true});
+}
