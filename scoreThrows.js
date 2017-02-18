a5065283dfa1c918cb806a810380faaf8ce57039
Edward Gao <egao@sent.com>
2017-02-18 09:56:14 -1000

    scoreThrows seems to be working but test not working

--- scoreThrows.js
+++ scoreThrows.js
@@ -3,7 +3,7 @@ module.exports = function(arr) {
 		if(rad < 5) prev.pts += 10;
 		if(rad >= 5 && rad <= 10) prev.pts += 5;
 		if(rad > 10) prev.perfect = false;
-		if(idx = arr.length - 1) {
+		if(idx === arr.length - 1) {
 			if(prev.perfect) prev.pts += 100;
 			return prev.pts;
 		}
